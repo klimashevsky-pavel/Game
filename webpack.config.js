5aaa196041ab15470c7418e3e7bf7e3bf1adda6e
klimashevsky-pavel <pavel.shrw@gmail.com>
2017-12-16 00:54:55 +0300

    some commit

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,15 @@ module.exports = {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ],
       },
+      { 
+test: /\.(png|jpg|gif)$/, 
+use: [ 
+{ 
+loader: 'file-loader', 
+options: { name: './img/[name].[ext]'} 
+} 
+] 
+},
       {
         test: /\.(png|jpg|gif)$/,
         use: ['url-loader']
