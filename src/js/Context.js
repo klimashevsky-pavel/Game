7c7dffa9eff45f35881710ddc81ce0aabe688ac4
klimashevsky-pavel <pavel.shrw@gmail.com>
2017-12-15 23:13:55 +0300

    removed src

--- src/js/Context.js
+++
@@ -1,9 +0,0 @@
-export let Context = {
-	canvas: null,
-	context: null,
-	create: function(){
-		this.canvas = document.getElementById('canvas');
-		this.context = this.canvas.getContext('2d');
-		return this.context;
-	}
-}
--- src/js/Lightning.js
+++
@@ -1,8 +0,0 @@
-import {Sprite} from './Sprite.js';
-
-let titanPath = '../pictures/titan.png';
-export let Lightning = function(){
- 	this.x = -1000;
-	this.y = -1000;
- 	this.sprite = new Sprite(titanPath, false, true, 120, 90, 70, 70)
- }
